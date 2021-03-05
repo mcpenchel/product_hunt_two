c4706664f8242b4d32dfaa69499d37911e6ce40d
Matheus Penchel <matheus.penchel@gmail.com>
2021-03-05 02:56:18 -0300

    testing the lecture's code with travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'products#index' # And remove the generated route `products/index`
+
+  resources :products, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
